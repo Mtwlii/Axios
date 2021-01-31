c5b43a158cdafa8a6618e115a81356fb27b28513
Mtwlii <Mtwliii999@gmail.com>
2021-01-31 05:07:17 +0200

    Json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+class Home extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    posts: res.data.slice(30,60)
+                })
+            })
+    }
+    render() {
+        const {posts} = this.state;
+        const postList = posts.length ? (
+            posts.map(
+                post=>{
+                    return(
+                        <div className="post card" key={post.id}>
+                            <div className="card-body"> 
+                                <span className="card-title">{post.title}</span>
+                                <p>{post.body}</p>
+                            </div>
+                        </div>
+                    )
+                }
+            )
+        ) :(
+            <div className="text-center">No Posts Yet</div>
+        )
+        return (
+            <div className="container">
+                <h4 className="text-center">Home</h4>
+                    {postList}
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="text-center">Home</h4>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur beatae
-                magnam expedita tempore repellendus quasi natus. Aliquid voluptas ipsa aliquam?
-            </p>
-
-        </div>
-    )
+            </div>
+        )
+    }
 }
 export default Home;
